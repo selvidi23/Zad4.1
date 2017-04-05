9e5b03d7c55f9118a0038d8e60e84bb40fec5461
selvidi23 <Hubert.Wawszczak@live.umcs.edu.pl>
2017-04-05 09:20:28 +0200

    sd

--- Zad4/app/src/main/java/a/zad4/Quiz.java
+++ Zad4/app/src/main/java/a/zad4/Quiz.java
@@ -2,15 +2,25 @@ package a.zad4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by Hubert on 04.04.2017.
  */
 
 public class Quiz extends Activity {
+
+    RadioGroup radioGroup1 = (RadioGroup)findViewById(R.id.rGroup1);
+    RadioButton radiobron1 = (RadioButton)findViewById(R.id.rButton11);
+    RadioButton radiobron2 = (RadioButton)findViewById(R.id.rButton12);
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz);
     }
+
+
 }
+
